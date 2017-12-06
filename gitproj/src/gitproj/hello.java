6c0405890f010159a0a42a0ef35768e01c32a100
su <su@text.com>
2017-12-05 22:39:48 -0800

    gitproj

---
+++ gitproj/src/gitproj/hello.java
@@ -0,0 +1,11 @@
+package gitproj;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is a jenkin build 1");
+
+	}
+
+}
