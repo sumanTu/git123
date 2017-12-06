409c55d9759a399bcf2fe7408171c600dc2d0357
su <su@text.com>
2017-12-05 23:03:46 -0800

    as

--- gitproj/src/gitproj/hello.java
+++ gitproj/src/gitproj/hello.java
@@ -4,7 +4,7 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is a jenkin build 5o");
+		System.out.println("this is a jenkin build 5o7");
 
 	}
 
