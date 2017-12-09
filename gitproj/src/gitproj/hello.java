743733aabd9b00456ed90ebca9d4f386fec952d2
su <su@text.com>
2017-12-09 11:31:41 -0800

    ada

--- gitproj/src/gitproj/hello.java
+++ gitproj/src/gitproj/hello.java
@@ -4,7 +4,7 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is a jen00kin build jfdkdj5o78455 one place git 1");
+		System.out.println("HEAD");
 	}
 
 }
