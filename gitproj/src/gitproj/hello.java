7941bd50acf0f25c1e9da8d5bf3f4af43c68b6a4
su <su@text.com>
2017-12-09 07:56:55 -0800

    commit

--- gitproj/src/gitproj/hello.java
+++ gitproj/src/gitproj/hello.java
@@ -4,8 +4,7 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is a jen00kin build 5o78455 one place");
-
+		System.out.println("this is a jen00kin build 5o78455 one place mallesh");
 	}
 
 }
